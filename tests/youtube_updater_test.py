b34eef05e5238d49f9839ea77a55baf88c15f324
yukkun007 <kato.yutaka@gmail.com>
2020-02-28 07:37:22 +0900

    テストの動画idを変更

--- tests/youtube_updater_test.py
+++ tests/youtube_updater_test.py
@@ -5,6 +5,6 @@ from mmyoutube.youtube_updater import add_to_play_list, delete_from_play_list
 class TestYoutubeUpdater:
     @pytest.mark.slow
     def test_add_to_play_list(self):
-        item_id = add_to_play_list("PLfZruavKtKZy0JdMo7PnbHAEo32twXVZl", "4eqSa9CzSAQ")
+        item_id = add_to_play_list("PLfZruavKtKZy0JdMo7PnbHAEo32twXVZl", "bRGeqa3ISEE")
         # すぐ削除
         delete_from_play_list(item_id)
